2a7d6fe47906a4730ee8162e855f1d6c59247686
George Sundarapandian David <437308@cognizant.com>
2021-01-09 17:00:14 -0600

    second commit by George on 01/09/2021

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace DockerAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Parameter1", "Parameter2" };
         }
 
         // GET api/values/5
